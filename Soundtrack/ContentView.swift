37ef2b76355dfbfe15e799f62768c0d6d1380cc5
bbarragancid23 <bbarragancid23@uchicago.edu>
2026-06-22 21:15:37 -0500

    Initial Commit

---
+++ Soundtrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Soundtrack
+//
+//  Created by Brian Barragan-Cid on 6/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Soundtrack/SoundtrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundtrackApp.swift
+//  Soundtrack
+//
+//  Created by Brian Barragan-Cid on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SoundtrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
